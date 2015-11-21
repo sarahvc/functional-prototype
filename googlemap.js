271779d07c5c58959120da325bfe252f42af116d
sarahvc <sarahvc@qq.com>
2015-11-20 23:55:05 -0500

    Update googlemap.js

--- googlemap.js
+++ googlemap.js
@@ -1,11 +1,3 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
